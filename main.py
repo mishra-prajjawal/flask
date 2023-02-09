768ea3e2196da336e6b60f06dd68340315d1571b
Prajjawal Mishra <94694798+mishra-prajjawal@users.noreply.github.com>
2023-02-09 07:52:45 +0000

    changes in main (/index)

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ users = {
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/index')
+def index():
+    return render_template('index.html')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
